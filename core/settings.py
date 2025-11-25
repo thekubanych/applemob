0856d59403f3717893dedfddff43eb28850c8dd5
thekubanych <kubanychmuhtarov>
2025-11-25 15:27:11 +0600

    Add superuser creation command

--- core/settings.py
+++ core/settings.py
@@ -2,20 +2,13 @@ from pathlib import Path
 import os
 from decouple import config
 
-
 # Пути проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Безопасность
-SECRET_KEY = 'django-insecure-+4_8yd)_@s!6)y^gk#e8z8@bedu^re%h3uu-_q8$(chyha(yum)'
-
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-+4_8yd)_@s!6)y^gk#e8z8@bedu^re%h3uu-_q8$(chyha(yum)')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['applemob.onrender.com', 'localhost', '127.0.0.1']
-
-# Статика
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Приложения
 INSTALLED_APPS = [
@@ -28,15 +21,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'corsheaders',
-
     'rest_framework',
-
     'api',
     'users',
 ]
+
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # ДОЛЖЕН БЫТЬ ПОСЛЕ SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 # URL конфигурация
 ROOT_URLCONF = 'core.urls'
@@ -69,7 +61,7 @@ TEMPLATES = [
 # WSGI
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# База данных (SQLite для простого старта)
+# База данных
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,6 +84,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Статика
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Кастомная модель пользователя
 AUTH_USER_MODEL = 'users.User'
@@ -99,42 +94,10 @@ AUTH_USER_MODEL = 'users.User'
 # Авто-поля
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Настройки почты для разработки (выводит письма в консоль)
+# Настройки почты
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'kubanychmuhtarov@gmail.com'
 
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'kubanychmuhtarov@gmail.com'
-# EMAIL_HOST_PASSWORD = 'yqfr dluj opnx kyyj'  # пароль для приложений
-#
-
-#
-# INSTALLED_APPS += ['anymail']
-#
-# EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
-# ANYMAIL = {
-#     "SENDGRID_API_KEY": "твой_sendgrid_api_key",
-# }
-# DEFAULT_FROM_EMAIL = "kubanychmuhtarov@gmail.com"
+# Безопасность cookies
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-# Авто-создание суперпользователя
-import os
-
-if not os.environ.get('SUPERUSER_CREATED'):
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
-
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='admin',
-            email='kubanychmuhtarov@gmail.com',
-            password='1234'  # Поменяйте пароль!
-        )
-        os.environ['SUPERUSER_CREATED'] = '1'
